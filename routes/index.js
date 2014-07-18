08eb77ab7bd8b777f299192e4d650e7b0cba1524
Simon Morley <simon@polkaspots.com>
2014-07-18 15:44:57 +0100

    removed chevron

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET feeutres page. */
+router.get('/features', function(req, res) {
+  res.render('features', { title: 'Features' });
+});
+
 module.exports = router;
