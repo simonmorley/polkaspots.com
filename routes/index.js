5f50e00395f7ee8cefd8171559c31afa0d91c513
LydiaCurtis <lydiacurtis@lydias-air.private>
2014-07-22 18:11:32 +0100

    Redirect URL Fix
    polkaspots.com/support wasn’t working.

--- routes/index.js
+++ routes/index.js
@@ -150,7 +150,7 @@ router.get('/support.html', function(req, res) {
   res.end();
 });
 
-router.get('/support/', function(req, res) {
+router.get('/support', function(req, res) {
   res.redirect(301, "https://polkaspots.zendesk.com/hc/en-us");
   res.end();
 });
