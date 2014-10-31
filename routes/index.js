da598880c3f5ee775794764aa2e5f12bbbdc5e3a
LydiaCurtis <lydiacurtis@Lydias-MacBook-Air.local>
2014-10-31 16:21:15 +0000

    redirect

--- routes/index.js
+++ routes/index.js
@@ -539,5 +539,9 @@ router.get('/meraki-captive-portal-setup', function(req, res) {
   res.end();
 });
 
+router.get('/how-it-works', function(req, res) {
+  res.redirect(301, "http://polkaspots.com/");
+  res.end();
+
 module.exports = router;
 
